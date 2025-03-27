9ce5e3f07e8cb735113bac9929572f7546d6b26b
sakshi Dhenge <dhengesakshi@gmail.com>
2025-03-27 23:46:13 +0530

    database code change and env file change

--- connection/database.js
+++ connection/database.js
@@ -1,18 +1,21 @@
 import mongoose from "mongoose";
 import chalk from "chalk";
-
 import dotenv from "dotenv";
+
 dotenv.config();
+
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URL);
-    // console.log(conn);
-    console.log(
-      chalk.bgMagenta.white(`Connected to MongoDB ${conn.connection.host}`)
-    );
+    const mongoUri = process.env.MONGO_URL_DB; 
+
+    await mongoose.connect(mongoUri); 
+
+    console.log(chalk.green("✅ MongoDB Connected Successfully"));
   } catch (error) {
-    console.error(chalk.bgRed.white(`Error in connection ${error}`));
+    console.error(chalk.bgRed.white(`❌ Error in connection: ${error.message}`));
+    process.exit(1); // Exit process on failure
   }
 };
-export default connectDB;
+
+export default connectDB;
